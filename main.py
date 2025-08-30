6ba9a7d68f99c417047a53eb6690f7cc143bd80d
Heshani833 <heshanishehana123@gmail.com>
2025-08-30 17:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,25 @@ options = [
 
 ]
 
+score = 0
+
+def check_answer(user_guess, correct_answer):
+    if user_guess == correct_answer:
+        return True
+    else:
+        return False
+
 for question_num in range(len(Questions_List)) : # 0 1 2 3 4
     print("************************")
     print(Questions_List[question_num]["text"])
     for i in options[question_num] :
         print(i)
+
+    guess = input("Enter (A/B/C/D) : ").upper()  
+    is_correct = check_answer(guess, Questions_List[question_num]["answer"])
+    if is_correct:
+        print("You got it right!")
+        score += 1
+    else:
+        print("Incorrect!")
+        print(f"The correct answer is {Questions_List[question_num]['answer']}")
